ab976cb04302cd443a5427c2d53990943d291f18
s22615 <s22615@pjwstk.edu.pl>
2024-04-26 18:52:29 +0200

    PAMO3 - done

---
+++ PAMO3/app/src/main/java/com/example/pamo2/ui/recipes/RecipesFragment.java
@@ -0,0 +1,38 @@
+package com.example.pamo2.ui.recipes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.pamo2.databinding.FragmentRecipesBinding;
+
+public class RecipesFragment extends Fragment {
+
+    private FragmentRecipesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        RecipesViewModel recipesViewModel =
+                new ViewModelProvider(this).get(RecipesViewModel.class);
+
+        binding = FragmentRecipesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textRecipes;
+        recipesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
